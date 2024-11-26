36e1c406404acd4aa8b3793103112e67bcfedef5
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-11-26 17:50:52 +0530

    utils code discussion

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -12,13 +12,15 @@ const asyncHandler = (requestHandler) =>{
 
 
 
-const asyncHandler = (fn) =>async() =>(req,res,next)  {
-    try{
-    await fn(req,res,next)
-    } catch(error){
-        res.status(err.code \\ 500 ).json({
-            success : false,
-            message : err.message   
-        })
-    }
-}  
+// const asyncHandler = (fn) =>async() =>(req,res,next)  {
+//     try{
+//     await fn(req,res,next)
+//     } catch(error){
+//         res.status(err.code \\ 500 ).json({
+//             success : false,
+//             message : err.message   
+//         })
+//     }
+// }  
+
+export {asyncHandler}
